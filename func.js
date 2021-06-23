f1b016c22be8b40101e8587916c86f6f7406ffc4
kitbull1 <80482480+kitbull1@users.noreply.github.com>
2021-06-23 16:22:33 +0530

    Add files via upload

---
+++ func.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+  let x = a + b;
+  console.log(x);
+}
+function hotjar(id, sv) {
+  (function (h, o, t, j, a, r) {
+    h.hj =
+      h.hj ||
+      function () {
+        (h.hj.q = h.hj.q || []).push(arguments);
+      };
+    h._hjSettings = { hjid: id, hjsv: sv };
+    a = o.getElementsByTagName("head")[0];
+    r = o.createElement("script");
+    r.async = 1;
+    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+    a.appendChild(r);
+  })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
+}
